8c254612e475b3ed3142b415b7e8b9faee04ed22
Ramil Galikeev <galikeevramil95@gmail.com>
2022-01-21 10:12:04 +0500

    changed styles

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,11 @@
+import AppHeader from "../appHeader/AppHeader";
 import UserProfile from "../userProfile/UserProfile";
 import BeerProfile from "../beerProfile/BeerProfile";
 
 const App = () => {
 	return (
 		<div className="app">
+			<AppHeader/>
 			<div className="container">
 				<UserProfile/>
 				<BeerProfile/>
